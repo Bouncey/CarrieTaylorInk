47fe842a832e98d392a3c04086524bb9cfa07970
Stuart Taylor <staylor1012@gmail.com>
2018-01-21 12:15:21 +0000

    Add source-filesystem and transform-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,24 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'blog',
+        path: `${__dirname}/src/resource/blog`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: `${__dirname}/src/pages`
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark'
+    }
+  ],
 };
